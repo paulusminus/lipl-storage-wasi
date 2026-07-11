bc9bcd52f251dec954a24c0d24f3b7911e7dbde5
Paul Min <info@paulmin.nl>
2026-07-11 15:12:02 +0200

    chore: update dependencies

--- crates/console-logger/src/lib.rs
+++ crates/console-logger/src/lib.rs
@@ -1,13 +1,37 @@
+use std::fmt::Display;
+
 use bindings::exports::wasi::logging::logging::Level;
+use bindings::wasi::cli::stdout;
 use bindings::wasi::clocks::system_clock::now;
+use wit_bindgen::spawn_local;
+
+use crate::bindings::wasi::cli::stderr;
+use crate::bindings::wit_stream;
 
 mod bindings {
     wit_bindgen::generate!({ world: "exports", with: {
         "wasi:clocks/system-clock@0.3.0": generate,
         "wasi:clocks/types@0.3.0": generate,
+        "wasi:cli/types@0.3.0": generate,
+        "wasi:cli/stdout@0.3.0": generate,
+        "wasi:cli/stderr@0.3.0": generate,
     } });
     use super::Component;
     export!(Component);
+}
+
+fn println<D: Display>(d: D, is_error: bool) {
+    let data = format!("{}\n", d).as_bytes().to_vec();
+    let (mut writer, reader) = wit_stream::new::<u8>();
+    spawn_local(async move {
+        writer.write_all(data).await;
+        drop(writer);
+    });
+    if is_error {
+        stdout::write_via_stream(reader);
+    } else {
+        stderr::write_via_stream(reader);
+    }
 }
 
 struct Component;
@@ -31,6 +55,6 @@ impl bindings::exports::wasi::logging::logging::Guest for Component {
         let timestamp = std::time::UNIX_EPOCH + duration;
         let timestamp = chrono::DateTime::<chrono::Utc>::from(timestamp);
 
-        println!("{}: {} {}", context, timestamp, message);
+        println(format!("{}: {} {}", context, timestamp, message), false);
     }
 }
